aa76678d8f525e3f09132ecd67fa69cc8ab14234
RakanAlamri <Rakanhalamri@gmail.com>
2022-10-11 15:33:55 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,6 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(
     title: 'Zaad',
-    home: Home(),
+    home: Login(),
   ));
 }
